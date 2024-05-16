baa837ffbd576215bcaddebd6651c378a5a57b9c
Fabio <cassisafabio97@gmail.com>
2024-05-16 17:50:34 +0200

    using getUserMedia method, .then(), .catch()

---
+++ script.js
@@ -0,0 +1,13 @@
+const video = document.getElementById('video');
+
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            video.srcObject = stream;
+        })
+        .catch(err => {
+            console.error('Error accessing the camera: ', err);
+        });
+}
+
+startVideo();
